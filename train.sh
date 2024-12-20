5c2ec0332dc6a82c6db8cc7bba9c459891efec27
LuckySJTU <942546416@qq.com>
2024-12-20 10:45:38 +0800

    1219_CBDIM256_EPOCH10, config isolation

--- train.sh
+++ train.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-RUN_NAME=1219_CBDIM768_EPOCH10
-MODEL_CONFIG_DIR=./conf/models/learnableVQ.yaml
+RUN_NAME=1219_CBDIM256_EPOCH10
+MODEL_CONFIG_DIR=./conf/models/vectorquantize.yaml
 DATA_CONFIG_DIR=./conf/data/example.yaml
 OUTPUT_DIR=./exp/$RUN_NAME
 
@@ -11,7 +11,7 @@ mkdir -p $OUTPUT_DIR
 cp $MODEL_CONFIG_DIR $OUTPUT_DIR/model_config.yaml
 cp $DATA_CONFIG_DIR $OUTPUT_DIR/data_config.yaml
 
-python train_vq.py --data_config $DATA_CONFIG_DIR --model_config $MODEL_CONFIG_DIR --ckpt_dir $OUTPUT_DIR 2>&1 |tee $OUTPUT_DIR/train.log
+python train_vq.py --data_config $OUTPUT_DIR/data_config.yaml --model_config $OUTPUT_DIR/model_config.yaml --ckpt_dir $OUTPUT_DIR 2>&1 |tee $OUTPUT_DIR/train.log
 
 # test
 # python train_vq.py --data_config $DATA_CONFIG_DIR --model_config $MODEL_CONFIG_DIR --ckpt_dir $OUTPUT_DIR --test
