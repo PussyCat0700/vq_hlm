6a6edc8a46d8273909aafacc1f6d4a37c9213d41
LuckySJTU <942546416@qq.com>
2024-12-21 13:03:12 +0800

    1219_lowdim_CBDIM8

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-RUN_NAME=1219_CBDIM1024_EPOCH10
+RUN_NAME=1219_lowdim_CBDIM8
 MODEL_CONFIG_DIR=./conf/models/vectorquantize.yaml
 DATA_CONFIG_DIR=./conf/data/example.yaml
 OUTPUT_DIR=./exp/$RUN_NAME
