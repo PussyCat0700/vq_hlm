8dbeec18e8a139d0805bc5998fe67b515c1578d3
LuckySJTU <942546416@qq.com>
2024-12-22 20:37:44 +0800

    test for latest ckpt

--- train.sh
+++ train.sh
@@ -2,16 +2,16 @@
 
 set -x
 
-RUN_NAME=1221_rvq_CB4_CBDIM1024_BS256
+RUN_NAME=1219_lowdim_CBDIM32
 MODEL_CONFIG_DIR=./conf/models/residualvq.yaml
 DATA_CONFIG_DIR=./conf/data/example.yaml
 OUTPUT_DIR=./exp/$RUN_NAME
 
-mkdir -p $OUTPUT_DIR
-cp $MODEL_CONFIG_DIR $OUTPUT_DIR/model_config.yaml
-cp $DATA_CONFIG_DIR $OUTPUT_DIR/data_config.yaml
+# mkdir -p $OUTPUT_DIR
+# cp $MODEL_CONFIG_DIR $OUTPUT_DIR/model_config.yaml
+# cp $DATA_CONFIG_DIR $OUTPUT_DIR/data_config.yaml
 
-python train_vq.py --data_config $OUTPUT_DIR/data_config.yaml --model_config $OUTPUT_DIR/model_config.yaml --ckpt_dir $OUTPUT_DIR 2>&1 |tee $OUTPUT_DIR/train.log
+# python train_vq.py --data_config $OUTPUT_DIR/data_config.yaml --model_config $OUTPUT_DIR/model_config.yaml --ckpt_dir $OUTPUT_DIR 2>&1 |tee $OUTPUT_DIR/train.log
 
 # test
-# python train_vq.py --data_config $DATA_CONFIG_DIR --model_config $MODEL_CONFIG_DIR --ckpt_dir $OUTPUT_DIR --test
+python train_vq.py --data_config $OUTPUT_DIR/data_config.yaml --model_config $OUTPUT_DIR/model_config.yaml --ckpt_dir $OUTPUT_DIR --test 2>&1 |tee $OUTPUT_DIR/test.log
