9d4ad2f9eb0af8bd4403c88687f936830cc1a227
LuckySJTU <942546416@qq.com>
2024-12-26 18:26:47 +0800

    1226_rvq_CB4_CBDIM128

--- train.sh
+++ train.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-RUN_NAME=1219_lowdim_CBDIM64
-MODEL_CONFIG_DIR=./conf/models/vectorquantize.yaml
+RUN_NAME=1226_rvq_CB4_CBDIM128
+MODEL_CONFIG_DIR=./conf/models/residualvq.yaml
 DATA_CONFIG_DIR=./conf/data/example.yaml
 OUTPUT_DIR=./exp/$RUN_NAME
 
@@ -11,7 +11,7 @@ mkdir -p $OUTPUT_DIR
 cp $MODEL_CONFIG_DIR $OUTPUT_DIR/model_config.yaml
 cp $DATA_CONFIG_DIR $OUTPUT_DIR/data_config.yaml
 
-python train_vq.py --data_config $OUTPUT_DIR/data_config.yaml --model_config $OUTPUT_DIR/model_config.yaml --ckpt_dir $OUTPUT_DIR 2>&1 |tee $OUTPUT_DIR/train.log
+# python train_vq.py --data_config $OUTPUT_DIR/data_config.yaml --model_config $OUTPUT_DIR/model_config.yaml --ckpt_dir $OUTPUT_DIR 2>&1 |tee $OUTPUT_DIR/train.log
 
 # test
 # python train_vq.py --data_config $OUTPUT_DIR/data_config.yaml --model_config $OUTPUT_DIR/model_config.yaml --ckpt_dir $OUTPUT_DIR --test 2>&1 |tee $OUTPUT_DIR/test.log
