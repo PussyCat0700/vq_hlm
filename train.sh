8672b805c1c700d838ac623b3913249e2fb7131c
LuckySJTU <942546416@qq.com>
2024-12-26 18:30:42 +0800

    1226_rvq_CB12_CBDIM256

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-RUN_NAME=1226_rvq_CB12_CBDIM128
+RUN_NAME=1226_rvq_CB12_CBDIM256
 MODEL_CONFIG_DIR=./conf/models/residualvq.yaml
 DATA_CONFIG_DIR=./conf/data/example.yaml
 OUTPUT_DIR=./exp/$RUN_NAME
