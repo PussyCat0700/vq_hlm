0f526ab2a40e1cfb9b2acc58555dc8a60b274045
LuckySJTU <942546416@qq.com>
2024-12-26 18:46:03 +0800

    slurm batch apply

--- train.sh
+++ train.sh
@@ -2,16 +2,17 @@
 
 set -x
 
-RUN_NAME=1226_rvq_CB12_CBDIM768
-MODEL_CONFIG_DIR=./conf/models/learnableRVQ.yaml
-DATA_CONFIG_DIR=./conf/data/example.yaml
+RUN_NAME=$1
+# RUN_NAME=1226_rvq_CB4_CBDIM128
+# MODEL_CONFIG_DIR=./conf/models/learnableRVQ.yaml
+# DATA_CONFIG_DIR=./conf/data/example.yaml
 OUTPUT_DIR=./exp/$RUN_NAME
 
-mkdir -p $OUTPUT_DIR
-cp $MODEL_CONFIG_DIR $OUTPUT_DIR/model_config.yaml
-cp $DATA_CONFIG_DIR $OUTPUT_DIR/data_config.yaml
+# mkdir -p $OUTPUT_DIR
+# cp $MODEL_CONFIG_DIR $OUTPUT_DIR/model_config.yaml
+# cp $DATA_CONFIG_DIR $OUTPUT_DIR/data_config.yaml
 
-# python train_vq.py --data_config $OUTPUT_DIR/data_config.yaml --model_config $OUTPUT_DIR/model_config.yaml --ckpt_dir $OUTPUT_DIR 2>&1 |tee $OUTPUT_DIR/train.log
+python train_vq.py --data_config $OUTPUT_DIR/data_config.yaml --model_config $OUTPUT_DIR/model_config.yaml --ckpt_dir $OUTPUT_DIR 2>&1 |tee $OUTPUT_DIR/train.log
 
 # test
-# python train_vq.py --data_config $OUTPUT_DIR/data_config.yaml --model_config $OUTPUT_DIR/model_config.yaml --ckpt_dir $OUTPUT_DIR --test 2>&1 |tee $OUTPUT_DIR/test.log
+python train_vq.py --data_config $OUTPUT_DIR/data_config.yaml --model_config $OUTPUT_DIR/model_config.yaml --ckpt_dir $OUTPUT_DIR --test 2>&1 |tee $OUTPUT_DIR/test.log
