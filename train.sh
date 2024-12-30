2ed3f85705c2a50ce68fe3d80be6b7c3262709c1
LuckySJTU <942546416@qq.com>
2024-12-30 09:35:41 +0800

    1230_rvq_CB12_CBDIM1024_CBSIZE2048_EPOCH30

--- train.sh
+++ train.sh
@@ -2,15 +2,15 @@
 
 set -x
 
-RUN_NAME=$1
+# RUN_NAME=$1
+RUN_NAME=1230_rvq_CB12_CBDIM1024_CBSIZE2048
+MODEL_CONFIG_DIR=./conf/models/residualvq.yaml
+DATA_CONFIG_DIR=./conf/data/example.yaml
 OUTPUT_DIR=./exp/$RUN_NAME
-# RUN_NAME=1226_rvq_CB12_CBDIM768_EPOCH10
-# MODEL_CONFIG_DIR=./conf/models/learnableRVQ.yaml
-# DATA_CONFIG_DIR=./conf/data/example.yaml
 
-# mkdir -p $OUTPUT_DIR
-# cp $MODEL_CONFIG_DIR $OUTPUT_DIR/model_config.yaml
-# cp $DATA_CONFIG_DIR $OUTPUT_DIR/data_config.yaml
+mkdir -p $OUTPUT_DIR
+cp $MODEL_CONFIG_DIR $OUTPUT_DIR/model_config.yaml
+cp $DATA_CONFIG_DIR $OUTPUT_DIR/data_config.yaml
 
 python train_vq.py --data_config $OUTPUT_DIR/data_config.yaml --model_config $OUTPUT_DIR/model_config.yaml --ckpt_dir $OUTPUT_DIR 2>&1 |tee $OUTPUT_DIR/train.log
 
